874b6a36f22ebcb351a675fd9edac95b2ee20aab
Kevin Schweikert <kevin@kevinschweikert.de>
2024-02-07 21:46:04 +0100

    add ollama integration to nvim

---
+++ files/nvim/lua/plugins/gen.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "David-Kunz/gen.nvim",
+    opts = {
+      model = "codellama", -- The default model to use.
+      display_mode = "float", -- The display mode. Can be "float" or "split".
+    },
+  },
+}
