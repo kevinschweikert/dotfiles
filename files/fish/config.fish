4d4977e51892da367411a7cee0ad2ab8549f8fd1
Kevin Schweikert <kevin@kevinschweikert.de>
2024-04-15 12:35:39 +0200

    init fzf in fish

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -45,3 +45,4 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 starship init fish | source
 zoxide init --cmd cd fish | source
 zellij setup --generate-completion fish | source
+fzf --fish | source
