7b0505ae305c14c27a88c432c55755c29eff714f
Kevin Schweikert <kevin@kevinschweikert.de>
2024-06-07 12:01:18 +0200

    get rid of deprecated nvim config

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -35,7 +35,6 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
-alias lvim="NVIM_APPNAME=lvim nvim"
 
 source $XDG_CONFIG_HOME/fish/colors/tokyonight.fish
 
--- files/nvim/lua/plugins/gen.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "David-Kunz/gen.nvim",
-    opts = {
-      model = "codellama", -- The default model to use.
-      display_mode = "float", -- The display mode. Can be "float" or "split".
-    },
-  },
-}
