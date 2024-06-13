bb49d184cbfb45d145524d8820603bc4b628140b
Kevin Schweikert <kevin@kevinschweikert.de>
2024-06-13 22:21:01 +0200

    update oil.nvim config

--- files/nvim/lua/plugins/oil.lua
+++ files/nvim/lua/plugins/oil.lua
@@ -5,6 +5,17 @@ return {
 		-- event = { "VeryLazy" },
 		opts = {
 			default_file_explorer = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				natural_order = true,
+				is_always_hidden = function(name, _)
+					return name == ".."
+				end,
+			},
+			win_options = {
+				wrap = true,
+			},
 			float = {
 				max_width = 100,
 				max_height = 30,
@@ -12,5 +23,16 @@ return {
 		},
 		-- Optional dependencies
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"chrishrb/gx.nvim",
+		keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+		cmd = { "Browse" },
+		init = function()
+			vim.g.netrw_nogx = 1 -- disable netrw gx
+		end,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		submodules = false, -- not needed, submodules are required only for tests
+		opts = {},
 	},
 }
