d1639ebf464d924732ea87aa3549f28ee7d01f62
Kevin Schweikert <kevin@kevinschweikert.de>
2024-06-13 22:21:56 +0200

    disable j and k in hardtime.nvim

--- files/nvim/lua/plugins/hardtime.lua
+++ files/nvim/lua/plugins/hardtime.lua
@@ -3,6 +3,13 @@ return {
 		"m4xshen/hardtime.nvim",
 		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 		event = "VeryLazy",
-		opts = { max_count = 10, disable_mouse = false },
+		opts = {
+			max_count = 10,
+			disable_mouse = false,
+			restricted_keys = {
+				["j"] = {},
+				["k"] = {},
+			},
+		},
 	},
 }
