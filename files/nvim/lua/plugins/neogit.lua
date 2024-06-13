27bebc0b4e1e3cc3a0a4f510a4358594a6b78949
Kevin Schweikert <kevin@kevinschweikert.de>
2024-06-13 22:22:55 +0200

    try out neogit

---
+++ files/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			-- "ibhagwan/fzf-lua",              -- optional
+		},
+		opts = {},
+		keys = { {
+			"<leader>n",
+			function()
+				require("neogit").open()
+			end,
+			desc = "Neogit",
+		} },
+	},
+}
