59cdd989d725381f86db788ece66d38cb3e804d7
Kevin Schweikert <kevin@kevinschweikert.de>
2024-08-16 10:45:20 +0200

    toggle diagnostic keymnap

--- files/nvim/lua/keymaps.lua
+++ files/nvim/lua/keymaps.lua
@@ -1,11 +1,15 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+local function toggle_diagnostics()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
+vim.keymap.set("n", "<leader>td", toggle_diagnostics, { desc = "[T]oggle [D]iagnostics" })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
