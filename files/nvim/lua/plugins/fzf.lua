877a67de24723b8b9d95c1ecaef5581f6b5166f3
Kevin Schweikert <kevin@kevinschweikert.de>
2024-09-16 16:02:47 +0200

    use dressing as proxy for vim.ui.select and vim.ui.input

--- files/nvim/lua/plugins/fzf.lua
+++ files/nvim/lua/plugins/fzf.lua
@@ -1,17 +1,13 @@
 return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "echasnovski/mini.icons" },
-	config = function(_, _)
-		local fzf = require("fzf-lua")
-		fzf.setup({
-			keymap = {
-				fzf = {
-					["ctrl-q"] = "select-all+accept",
-				},
+	opts = {
+		keymap = {
+			fzf = {
+				["ctrl-q"] = "select-all+accept",
 			},
-		})
-		fzf.register_ui_select()
-	end,
+		},
+	},
 	keys = {
 		{ "<leader><leader>", "<cmd>lua require('fzf-lua').files()<cr>", desc = "Find files" },
 		{ "<leader>fg", "<cmd>lua require('fzf-lua').live_grep()<cr>", desc = "Find word" },
