009ffe534a090006776b32bd8c57e8d927cd4cad
Kevin Schweikert <kevin@kevinschweikert.de>
2024-10-20 19:23:13 +0200

    make fzf project switcher more powerful

--- files/fish/functions/kt.fish
+++ files/fish/functions/kt.fish
@@ -1,8 +1,27 @@
 function kt -d "Kitty tab from dev projects"
-    set -l path $HOME/projects/{personal,imen}
+    set -l projects_root $HOME/projects
+    set -l path $projects_root/{personal,imen}
+    set -l find_cmd "fd --type d --max-depth 1 . $path"
+
     set -l selected_dir (
-        fd --type d --max-depth 1 . $path |
-        fzf-tmux -p --cycle --layout=reverse --height=50% --delimiter / --with-nth -3..-2 --preview='CLICOLOR_FORCE=1 glow -p -w 100 -s dark {}/README.md 2>/dev/null || tokei {}' --preview-window='70%' --prompt 't> '
+        eval $find_cmd | fzf-tmux \
+            -p \
+            --cycle \
+            --layout=reverse \
+            --height=50% \
+            --delimiter='/' \
+            --with-nth='-3..-2' \
+            --preview='CLICOLOR_FORCE=1 glow -p -w 100 -s dark {}/README.md 2>/dev/null || tokei {}' \
+            --preview-window='70%' \
+            --prompt 'project> ' \
+            --bind="ctrl-n:execute(mkdir -p (fd --type d --max-depth 1 . $projects_root | fzf)/{q})+reload($find_cmd)" \
+            --bind="del:execute(
+                        if test (read -P 'Are you sure you want to delete {}? (y/N) ') = 'y';
+                            rm -rf {};
+                        end)+reload($find_cmd)" \
+            --header-first \
+            --header "Ctrl-n: new folder; Del: delete folder"
+            
     )
     if test -n "$selected_dir"
         set -l tab_name (basename $selected_dir)
