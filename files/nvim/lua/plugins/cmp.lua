5752fd517c2459272df77b91f9c90ab3ee1a25aa
Kevin Schweikert <kevin@kevinschweikert.de>
2024-10-27 22:42:10 +0100

    disable friendly snippets, now that blink.cmp gets them from lsp

--- files/nvim/lua/plugins/cmp.lua
+++ files/nvim/lua/plugins/cmp.lua
@@ -3,7 +3,7 @@ return {
 		"saghen/blink.cmp",
 		lazy = false, -- lazy loading handled internally
 		-- optional: provides snippets for the snippet source
-		dependencies = "rafamadriz/friendly-snippets",
+		-- dependencies = "rafamadriz/friendly-snippets",
 
 		-- use a release tag to download pre-built binaries
 		version = "v0.*",
