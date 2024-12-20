32b711800ed92f7dbab0cfbb1e7d765beec3f7db
Kevin Schweikert <kevin@kevinschweikert.de>
2024-12-20 18:39:11 +0100

    update blink.cmp

--- files/nvim/lua/plugins/cmp.lua
+++ files/nvim/lua/plugins/cmp.lua
@@ -14,32 +14,18 @@ return {
 
 		opts = {
 			keymap = { preset = "default" },
-			highlight = {
-				-- sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- useful for when your theme doesn't support blink.cmp
-				-- will be removed in a future release, assuming themes add support
-				use_nvim_cmp_as_default = true,
-			},
-			windows = {
-				autocomplete = {
-					draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } } },
+			completion = {
+				menu = {
+					draw = {
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+					},
 				},
-				-- ghost_text = {
-				-- 	enabled = true,
-				-- },
 			},
 			sources = {
-				completion = {
-					enabled_providers = { "lsp", "path", "snippets", "buffer" },
-				},
+				default = { "lsp", "path", "snippets", "buffer" },
+				-- optionally disable cmdline completions
+				-- cmdline = {},
 			},
-			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
-			-- experimental auto-brackets support
-			accept = { auto_brackets = { enabled = true } },
-			-- experimental signature help support
-			-- trigger = { signature_help = { enabled = true } },
 		},
 	},
 }
