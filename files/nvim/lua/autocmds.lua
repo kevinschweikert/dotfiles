75e47d97d68b7cad104cda58fc59a53b38eb40b3
Kevin Schweikert <kevin@kevinschweikert.de>
2025-03-31 10:38:30 +0200

    improve foldexpr

--- files/nvim/lua/autocmds.lua
+++ files/nvim/lua/autocmds.lua
@@ -104,6 +104,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		if client:supports_method("textDocument/completion") then
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
+		if client:supports_method("textDocument/foldingRange") then
+			local win = vim.api.nvim_get_current_win()
+			vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+		end
 	end,
 })
 
